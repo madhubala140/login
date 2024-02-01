981076a4cd9274423983d11ff732c94dca9c62e9
madhubala140 <madhusekar.s1994@gmail.com>
2024-02-01 15:10:15 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const App = () => {
+	const navigate = useNavigate();
+	const [user, setUser] = useState({
+		userName: "",
+		password: "",
+	});
+	const [error, setError] = useState("");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (user.userName === "user" && user.password === "password") {
+			console.log("hi");
+			navigate("/welcome");
+		} else {
+			setError("Invalid username or password");
+		}
+	};
+
+	return (
+		<div>
+			<h1>Login Page</h1>
+			{error && (
+				<>
+					<div className="error">{error}</div>
+					<br />
+				</>
+			)}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">Username:</label>
+				<input
+					type="text"
+					name="username"
+					id="username"
+					value={user.userName}
+					onChange={(e) => setUser({ ...user, userName: e.target.value })}
+					required
+				/>
+				<br />
+				<label htmlFor="password">Password:</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={user.password}
+					onChange={(e) => setUser({ ...user, password: e.target.value })}
+					required
+				/>
+				<br />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+};
 
 export default App;
